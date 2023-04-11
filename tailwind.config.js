f165bf57a66bf78cb9b7620f3e632b3cd706527f
Thibault BRAVAR <tbravar@smc2.local>
2023-04-11 09:51:41 +0200

    third commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').config} */
+module.exports = {
+    content: [
+        'templates/**/*.html.twig',
+        'assets/scripts/*.js'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
